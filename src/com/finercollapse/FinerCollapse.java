fbd7c6611ce4f98dc2fcfc19de1f7694cbd8b9e5
Dave Fine <davef@bluemoon.(none)>
2010-01-17 23:00:43 -0500

    attempted to make tile view show up

--- src/com/finercollapse/FinerCollapse.java
+++ src/com/finercollapse/FinerCollapse.java
@@ -2,16 +2,13 @@ package com.finercollapse;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class FinerCollapse extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView tv = new TextView(this);
-        tv.setText("Hello World!");
-        setContentView(tv);
-        //setContentView(R.layout.main);
+
+        setContentView(R.layout.main);
     }
 }
